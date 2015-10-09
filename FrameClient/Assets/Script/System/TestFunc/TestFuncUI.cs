270837d1b61379c949c7fe83ca267c7d12765822
wuhuichun <1053071197@qq.com>
2015-10-09 21:36:56 +0800

    -- 新加功能测试UI -- 几个空场景

---
+++ FrameClient/Assets/Script/System/TestFunc/TestFuncUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestFuncUI : MonoBehaviour {
+
+    private Button BtnFunc01;
+    private Button BtnFunc02;
+    private Button BtnFunc03;
+    private Button BtnFunc04;
+
+
+    void Awake()
+    {
+        BtnFunc01 = transform.FindChild("BtnFunc01").GetComponent<Button>();
+        BtnFunc02 = transform.FindChild("BtnFunc02").GetComponent<Button>();
+        BtnFunc03 = transform.FindChild("BtnFunc03").GetComponent<Button>();
+        BtnFunc04 = transform.FindChild("BtnFunc04").GetComponent<Button>();
+
+        BtnFunc01.onClick.AddListener(OnBtnFunc01Click);
+        BtnFunc02.onClick.AddListener(OnBtnFunc02Click);
+        BtnFunc03.onClick.AddListener(OnBtnFunc03Click);
+        BtnFunc04.onClick.AddListener(OnBtnFunc04Click);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    private void OnBtnFunc01Click()
+    {
+        Debug.Log("Click BtnFunc01");
+        Application.LoadLevel("World");
+    }
+
+    private void OnBtnFunc02Click()
+    {
+        Debug.Log("Click BtnFunc02");
+        Application.LoadLevel("UITest");
+    }
+
+    private void OnBtnFunc03Click()
+    {
+        Debug.Log("Click BtnFunc03");
+        Application.LoadLevel("Create");
+    }
+
+    private void OnBtnFunc04Click()
+    {
+        Debug.Log("Click BtnFunc04");
+    }
+}
